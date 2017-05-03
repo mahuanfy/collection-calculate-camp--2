7b36fd270240f16c142c1684311eb80cdf6ced62
821453366 <821453366@qq.com>
2017-05-03 14:08:21 +0800

    20

--- practices/map/map_to_three_multiples.js
+++ practices/map/map_to_three_multiples.js
@@ -1,6 +1,10 @@
 'use strict';
-var map_to_three_multiples = function(collections){
-  return [3,9,15,12,27];
+var map_to_three_multiples = function (collections) {
+  var result = [];
+  for (var index in collections) {
+    result.push(collections[index] * 3);
+  }
+  return result;
 };
 
 module.exports = map_to_three_multiples;
