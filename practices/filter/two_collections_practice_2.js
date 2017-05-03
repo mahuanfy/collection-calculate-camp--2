0a279a39f239db56abef9d652aa5c926a13b9532
821453366 <821453366@qq.com>
2017-05-03 13:04:37 +0800

    14

--- practices/filter/two_collections_practice_2.js
+++ practices/filter/two_collections_practice_2.js
@@ -3,6 +3,20 @@
 function choose_no_common_elements(collection_a, collection_b) {
 
   //在这里写入代码
+  var result = [];
+  var count = 0;
+  for (var j = 0; j < collection_a.length; j++) {
+    for (var i = 0; i < collection_b.length; i++) {
+      if (collection_b[i] != collection_a[j]) {
+        count++;
+      }
+    }
+    if(count === collection_b.length){
+      result.push(collection_a[j]);
+    }
+    count=0;
+  }
+  return result;
 }
 
 module.exports = choose_no_common_elements;
