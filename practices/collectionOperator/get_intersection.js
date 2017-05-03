a33f2d7bccf97b15a60bf27c27b31c991d8bf821
821453366 <821453366@qq.com>
2017-05-03 10:06:43 +0800

    5

--- practices/collectionOperator/get_intersection.js
+++ practices/collectionOperator/get_intersection.js
@@ -2,6 +2,15 @@
 
 function get_intersection(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection_b.length; i++) {
+    for (var j = 0; j < collection_a.length; j++) {
+      if (collection_b[i] === collection_a[j]) {
+        result.push(collection_b[i])
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = get_intersection;
