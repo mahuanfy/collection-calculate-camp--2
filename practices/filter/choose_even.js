a2408d68af065d18c8e24dcb679190950aa35f7c
821453366 <821453366@qq.com>
2017-05-03 11:11:48 +0800

    9

--- practices/filter/choose_even.js
+++ practices/filter/choose_even.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function choose_even(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var index in collection)
+    if (collection[index] % 2 === 0) {
+      result.push(collection[index]);
+    }
+  return result;
 }
 
 module.exports = choose_even;
