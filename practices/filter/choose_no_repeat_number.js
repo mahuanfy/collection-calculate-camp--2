ce42ac8663a9f3b442dff11515ecc3d2e046f5ab
821453366 <821453366@qq.com>
2017-05-03 11:33:43 +0800

    11

--- practices/filter/choose_no_repeat_number.js
+++ practices/filter/choose_no_repeat_number.js
@@ -1,8 +1,10 @@
 'use strict';
 
 function choose_no_repeat_number(collection) {
-
   //在这里写入代码
+  var result = [];
+  result = Array.from(new Set(collection));
+  return result;
 }
 
 module.exports = choose_no_repeat_number;
