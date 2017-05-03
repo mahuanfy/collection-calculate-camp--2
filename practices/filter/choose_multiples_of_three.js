86f8866e2bc811ccdef180f06c09b435cd057950
821453366 <821453366@qq.com>
2017-05-03 11:21:09 +0800

    10

--- practices/filter/choose_multiples_of_three.js
+++ practices/filter/choose_multiples_of_three.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function choose_multiples_of_three(collection) {
-
   //在这里写入代码
+  var result = [];
+  for (var index in collection)
+    if (collection[index] % 3 === 0) {
+      result.push(collection[index]);
+    }
+  return result;
 }
 
 module.exports = choose_multiples_of_three;
