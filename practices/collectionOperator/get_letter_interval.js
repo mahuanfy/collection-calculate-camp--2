6e4b6ed5f9ba6a341953fb2a3af4c7eb049a6c45
821453366 <821453366@qq.com>
2017-05-03 09:45:41 +0800

    第三道

--- practices/collectionOperator/get_letter_interval.js
+++ practices/collectionOperator/get_letter_interval.js
@@ -2,6 +2,7 @@
 
 function get_letter_interval(number_a, number_b) {
   //在这里写入代码
+
 }
 
 module.exports = get_letter_interval;
--- specs/collectionOperator/collect_last_element_spec.js
+++ specs/collectionOperator/collect_last_element_spec.js
@@ -6,7 +6,7 @@ describe('collect_last_element', function () {
 
   var collection = [1, 2, 3, 4, 5];
 
-  iit('弹出集合最后一个元素', function() {
+  it('弹出集合最后一个元素', function() {
 
     var result = collect_last_element(collection);
     expect(result).toEqual(5);
