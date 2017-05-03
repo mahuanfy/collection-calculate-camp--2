82f88417d7f4aeccc40f868d932824b3bad4e535
821453366 <821453366@qq.com>
2017-05-03 14:34:28 +0800

    24

--- practices/map/rank_desc.js
+++ practices/map/rank_desc.js
@@ -1,6 +1,13 @@
 'use strict';
 var rank_desc = function(collection){
-  return [2,3,4,5,6];
+  function sortNumber(a, b) {
+    return a - b;
+  }
+  var result = [];
+  result = collection.sort(sortNumber);
+
+  return result;
+
 };
 
 module.exports = rank_desc;
