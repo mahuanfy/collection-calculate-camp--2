31093b1862c416459cd9c033fb6b6036eb537b24
821453366 <821453366@qq.com>
2017-05-03 12:56:45 +0800

    13

--- practices/filter/grouping_count.js
+++ practices/filter/grouping_count.js
@@ -3,6 +3,21 @@
 function grouping_count(collection) {
 
   //在这里写入代码
+// var result = new Map();
+// var result_num = [];
+// var collection_num=0;
+//
+//   result_num = Array.from(new Set(collection));
+//   for (var i = 0; i < result_num.length; i++) {
+//     for(var j =0; i<collection.length;j++) {
+//       if(result_num[i] === collection[j]){
+//         collection_num++;
+//       }
+//       result.set(result_num[i],collection_num);
+//     }
+//     collection_num=0;
+//   }
+//   return result;
 }
 
 module.exports = grouping_count;
