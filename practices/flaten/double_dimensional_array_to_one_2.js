59a37a514d31f81f1ea93f63207de3e536d60b5b
821453366 <821453366@qq.com>
2017-05-03 13:47:37 +0800

    17

--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,6 +3,15 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var result_sum = [];
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    for (var j = 0; j < collection[i].length; j++) {
+      result_sum.push(collection[i][j]);
+    }
+  }
+  result = Array.from(new Set(result_sum))
+  return result;
 }
 
 module.exports = double_to_one;
