896dffa3fa7b45e8661217e2212bfca6c4d9e974
821453366 <821453366@qq.com>
2017-05-03 11:03:41 +0800

    8

--- practices/collectionOperator/get_union.js
+++ practices/collectionOperator/get_union.js
@@ -2,6 +2,9 @@
 
 function get_union(collection_a, collection_b) {
   //在这里写入代码
+  var result = [];
+  result = Array.from(new Set(collection_a.concat(collection_b)));
+  return result;
 }
 
 module.exports = get_union;
