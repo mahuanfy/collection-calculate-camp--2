63c4209aa28682af24160852df0dc2ea93938f8f
821453366 <821453366@qq.com>
2017-05-03 14:12:03 +0800

    21

--- practices/map/number_map_to_word.js
+++ practices/map/number_map_to_word.js
@@ -1,6 +1,12 @@
 'use strict';
 var number_map_to_word = function(collection){
-  return ['a','b','c','d','e'];
+  var result = [];
+  var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+  for (var index in collection) {
+    result.push(letter[collection[index]]);
+  }
+  return result;
 };
 
 module.exports = number_map_to_word;
