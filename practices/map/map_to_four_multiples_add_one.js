d738228da3ab212c89281874259754c5a16af385
821453366 <821453366@qq.com>
2017-05-03 14:05:42 +0800

    19

--- practices/map/map_to_four_multiples_add_one.js
+++ practices/map/map_to_four_multiples_add_one.js
@@ -1,6 +1,10 @@
 'use strict';
-var map_to_four_multiples_add_one = function(collection){
-  return [5,9,13,17,21];
+var map_to_four_multiples_add_one = function (collection) {
+  var result = [];
+  for (var index in collection) {
+    result.push((collection[index] * 4) + 1);
+  }
+  return result;
 };
 
 module.exports = map_to_four_multiples_add_one;
