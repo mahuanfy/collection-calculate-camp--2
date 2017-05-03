605102c618ce3e0899723317a24a169b8db3ef7d
821453366 <821453366@qq.com>
2017-05-03 13:34:55 +0800

    16

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -3,6 +3,16 @@
 function double_to_one(collection) {
 
   //在这里写入代码
+  var result = [];
+  for (var i = 0; i < collection.length; i++) {
+    if (collection[i].length === undefined) {
+      result.push(collection[i]);
+    }
+    for (var j = 0; j < collection[i].length; j++) {
+      result.push(collection[i][j]);
+    }
+  }
+  return result;
 }
 
 module.exports = double_to_one;
