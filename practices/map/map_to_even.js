b649d54e083604ee666ca34a6262f8013c44f452
821453366 <821453366@qq.com>
2017-05-03 14:03:12 +0800

    18

--- practices/map/map_to_even.js
+++ practices/map/map_to_even.js
@@ -1,5 +1,10 @@
 'use strict';
-function map_to_even(collection){
-  return [2,4,6,8,10];
+function map_to_even(collection) {
+
+  var result = [];
+  for (var index in collection) {
+    result.push( collection[index]*2);
+  }
+  return result;
 }
 module.exports = map_to_even;
