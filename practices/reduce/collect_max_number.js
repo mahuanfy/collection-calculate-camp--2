3e92dd91af50b90ce31544b1b00d53f854e4c7b3
821453366 <821453366@qq.com>
2017-05-05 18:58:23 +0800

    25

--- practices/reduce/collect_max_number.js
+++ practices/reduce/collect_max_number.js
@@ -2,6 +2,13 @@
 
 function collect_max_number(collection) {
   //在这里写入代码
+  var result = collection[0];
+  for (var index in collection) {
+    if (collection[index] > result) {
+      result = (collection[index]);
+    }
+  }
+  return result;
 }
 
 module.exports = collect_max_number;
