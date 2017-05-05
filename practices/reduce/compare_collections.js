222ad16548aa79b22ec78f89246379d1c172d4b4
821453366 <821453366@qq.com>
2017-05-05 19:06:20 +0800

    27

--- practices/reduce/compare_collections.js
+++ practices/reduce/compare_collections.js
@@ -2,6 +2,11 @@
 
 function compare_collections(collection_a, collection_b) {
   //在这里写入代码
+
+  if(collection_a.toString() == collection_b.toString()){
+    return true;
+  }
+  return false;
 }
 
 module.exports = compare_collections;
