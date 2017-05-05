430b21b282716192b4b6756007937013b51705bc
821453366 <821453366@qq.com>
2017-05-05 19:11:21 +0800

    28

--- practices/reduce/compute_average.js
+++ practices/reduce/compute_average.js
@@ -2,6 +2,14 @@
 
 function compute_average(collection) {
   //在这里写入代码
+  var result = 0;
+  var sum = 0;
+  for (var index in collection) {
+    result += collection[index];
+    sum++;
+  }
+  result = result / sum;
+  return result;
 }
 
 module.exports = compute_average;
