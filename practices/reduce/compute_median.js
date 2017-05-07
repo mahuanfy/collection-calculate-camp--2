1e10dc31e8f331ea6ffbfc55079b448b0bf4933b
821453366 <821453366@qq.com>
2017-05-07 11:50:26 +0800

    30

--- practices/reduce/compute_median.js
+++ practices/reduce/compute_median.js
@@ -2,7 +2,20 @@
 
 function compute_median(collection) {
   //在这里写入代码
+  var result = [];
+  var number = 0;
+  result = collection.sort(sortNumber);
+  if (result.length % 2 === 0) {
+    number = (result[result.length / 2 - 1] + result[result.length / 2]) / 2;
+  } else {
+    number = result[Math.floor(result.length / 2)];
+  }
+  return number;
 }
+function sortNumber(a, b) {
+  return a - b;
+}
+
 
 module.exports = compute_median;
 
