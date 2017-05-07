f182569576b8d5db091cba3bbeaf5d565d3ab8aa
821453366 <821453366@qq.com>
2017-05-07 12:23:37 +0800

    34

--- practices/reduce/find_last_even.js
+++ practices/reduce/find_last_even.js
@@ -2,6 +2,14 @@
 
 function find_last_even(collection) {
   //在这里写入代码
+  var result = 0;
+  for (var index in collection) {
+    if (collection[index] % 2 === 0) {
+      result = collection[index];
+
+    }
+  }
+  return result;
 }
 
 module.exports = find_last_even;
