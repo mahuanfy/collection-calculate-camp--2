d65cba1f12f877302e2427bc44d30c7df0905ae3
821453366 <821453366@qq.com>
2017-05-07 12:01:49 +0800

    32

--- practices/reduce/find_element_last_subscript.js
+++ practices/reduce/find_element_last_subscript.js
@@ -2,6 +2,9 @@
 
 function calculate_elements_sum(collection, element) {
   //在这里写入代码
+  var result = 0;
+  result = collection.lastIndexOf(element);
+  return result;
 }
 
 module.exports = calculate_elements_sum;
