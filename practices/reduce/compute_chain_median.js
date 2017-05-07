d98ba81295767589b3437808543eaf83e0b7e6f7
821453366 <821453366@qq.com>
2017-05-07 11:36:44 +0800

    29

--- practices/reduce/compute_chain_median.js
+++ practices/reduce/compute_chain_median.js
@@ -2,6 +2,18 @@
 
 function compute_chain_median(collection) {
   //在这里写入代码
+  var result = [];
+  var resultString = [];
+  resultString = collection.split("->");
+  for (var index in resultString) {
+    result.push(parseInt(resultString[index]));
+  }
+  result = result.sort(sortNumber);
+  var number = (result[result.length / 2 - 1] + result[result.length / 2]) / 2;
+  return number;
+}
+function sortNumber(a, b) {
+  return a - b;
 }
 
 module.exports = compute_chain_median;
