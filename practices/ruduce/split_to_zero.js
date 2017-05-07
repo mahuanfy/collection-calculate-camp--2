aa6b251372f75bf2fc9ee70f375efde6a95cd67b
821453366 <821453366@qq.com>
2017-05-07 12:49:24 +0800

    35

--- practices/ruduce/split_to_zero.js
+++ practices/ruduce/split_to_zero.js
@@ -2,6 +2,17 @@
 
 function spilt_to_zero(number, interval) {
   //在这里写入代码
+  var result = [];
+  result.push(number);
+  while (number > 0) {
+    number = parseFloat((number - interval).toFixed(1));
+    if (number === 0.0) {
+      result.push(0)
+    } else {
+      result.push(number);
+    }
+  }
+  return result;
 }
 
 module.exports = spilt_to_zero;
