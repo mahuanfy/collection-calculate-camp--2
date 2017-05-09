bd74d460d4550ad1b1d79b1a203b498e503a860f
821453366 <821453366@qq.com>
2017-05-09 13:23:34 +0800

    提交第45题

--- practices/section_3/practice_3/practice.js
+++ practices/section_3/practice_3/practice.js
@@ -1,5 +1,36 @@
-function create_updated_collection(collection_a, object_b) {
-  //在这里写入代码
+function create_updated_collection(collection, object_b) {
+
+  var collection_same = [];
+  var key_value = "";
+  var count_value = 0;
+
+  for(var i = 0; i < collection.length;i++){
+    if(collection[i] != -1){
+      key_value = collection[i];
+      var keyCountObj = isExist( collection,key_value, count_value);
+      collection_same.push(keyCountObj);
+      count_value = 0;
+    }
+  }
+  for (var index in collection_same){
+    for(var index_b in object_b.value){
+      if(collection_same[index].key === object_b.value[index_b]){
+        collection_same[index].count-=Math.floor((collection_same[index].count)/3);
+      }
+    }
+  }
+  return collection_same;
+}
+
+function isExist(collection,key_value, count_value) {
+  for (var j = 0; j < collection.length; j++) {
+    if (key_value === collection[j]) {
+      count_value++;
+      collection[j] = -1;
+    }
+  }
+  var keyCountObj = {key: key_value, count: count_value};
+  return keyCountObj;
 }
 
 module.exports = create_updated_collection;
