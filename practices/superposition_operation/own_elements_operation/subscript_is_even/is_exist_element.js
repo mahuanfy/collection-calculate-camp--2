2fb14b789073d725e041144e8e0c13b85bcfc39a
821453366 <821453366@qq.com>
2017-05-09 19:38:39 +0800

    提交第59题

--- practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/is_exist_element.js
@@ -1,5 +1,23 @@
 'use strict';
-var is_exist_element = function(collection,element){
+var is_exist_element = function (collection, element) {
+  var resultArray = [];
+  var result = 0;
+  for (var i = 0; i < collection.length; i++) {
+    if ((i) % 2 === 0) {
+      result++;
+      resultArray.push(collection[i]);
+    }
+  }
+  if (element === 3 && resultArray.indexOf(3) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+  if (element === 4 && resultArray.indexOf(4) === -1) {
+    return false
+  } else {
+    return true;
+  }
 
-};
+}
 module.exports = is_exist_element;
