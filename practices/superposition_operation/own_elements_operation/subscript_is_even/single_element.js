92d956742aecc3db2f42c4143be42cf68f04a372
821453366 <821453366@qq.com>
2017-05-09 20:19:38 +0800

    60

--- practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/single_element.js
@@ -1,5 +1,30 @@
 'use strict';
-var single_element = function(collection){
+var single_element = function (collection) {
+  var resultArray = [];
+  var resultArray_a = [];
+  var result_a = [];
+  var num = 0;
 
+  var result = 0;
+  for (var i = 0; i < collection.length; i++) {
+    if ((i + 1) % 2 === 0) {
+      result++;
+      resultArray.push(collection[i]);
+    }
+  }
+  resultArray_a = Array.from(new Set(resultArray));
+  for (var index in resultArray_a) {
+
+    for (var i in resultArray) {
+      if (resultArray_a[index] === resultArray[i]) {
+        num++;
+      }
+    }
+    if (num === 1) {
+      result_a.push(resultArray_a[index]);
+    }
+    num = 0;
+  }
+  return result_a
 };
 module.exports = single_element;
