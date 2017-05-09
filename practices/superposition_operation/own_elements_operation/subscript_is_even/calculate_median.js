7ebbeab99a64fb88610683cdaa2e5253859295bd
821453366 <821453366@qq.com>
2017-05-09 16:44:47 +0800

    提交第57题

--- practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
+++ practices/superposition_operation/own_elements_operation/subscript_is_even/calculate_median.js
@@ -1,5 +1,17 @@
 'use strict';
-var calculate_median = function(collection){
-
-};
-module.exports = calculate_median;
+var calculate_median = function (collection) {
+  var resultArray = [];
+  var result = 0;
+  for (var i = 0; i < collection.length; i++) {
+    if ((i + 1) % 2 === 0) {
+      result++;
+      resultArray.push(collection[i]);
+    }
+  }
+  if (result % 2 !== 0) {
+    return resultArray[Math.ceil(resultArray.length / 2) - 1];
+  } else {
+    return (resultArray[Math.ceil(resultArray.length / 2)-1] + resultArray[Math.ceil(resultArray.length / 2)])/2;
+  }
+}
+  module.exports = calculate_median;
