5772b0ebf3d28df209706c3a0c490eb2d83ce8a3
821453366 <821453366@qq.com>
2017-05-09 15:11:06 +0800

    提交第51题

--- practices/superposition_operation/collection_operation/practice_3.js
+++ practices/superposition_operation/collection_operation/practice_3.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
+  var result = 0;
 
-  //在这里写入代码
+  for (var index in collection) {
+    if (collection[index] % 2 !== 0) {
+      result+=(collection[index]*3+5);
+    }
+  }
+  return result;
 }
 
 module.exports = hybrid_operation_to_uneven;
