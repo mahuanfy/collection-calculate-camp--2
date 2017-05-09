50e9c98e412a4d1fb1647b165374c070ea9aaea3
821453366 <821453366@qq.com>
2017-05-09 15:24:24 +0800

    提交第53题

--- practices/superposition_operation/interval_operation/average_uneven.js
+++ practices/superposition_operation/interval_operation/average_uneven.js
@@ -1,8 +1,17 @@
 'use strict';
 
 function average_uneven(collection) {
+  var sum = 0;
+  var num = 0;
 
-  //在这里写入代码
+  for (var index in collection) {
+    if (collection[index] % 2 !== 0) {
+      sum += collection[index];
+      num++;
+    }
+  }
+
+  return sum/num;
 }
 
 module.exports = average_uneven;
