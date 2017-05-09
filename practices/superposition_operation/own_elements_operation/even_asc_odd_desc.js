52bba54b85cc430d8fe1cbf329d20a83ff440451
821453366 <821453366@qq.com>
2017-05-09 21:17:33 +0800

    第61题目

--- practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
+++ practices/superposition_operation/own_elements_operation/even_asc_odd_desc.js
@@ -1,5 +1,24 @@
 'use strict';
-var even_asc_odd_desc = function(collection){
+var even_asc_odd_desc = function (collection) {
+  var resultArray_a = [];
+  var resultArray_b = [];
 
+  for (var i = 0; i < collection.length; i++) {
+    if ((collection[i]) % 2 === 0) {
+
+      resultArray_a.push(collection[i]);
+    } else {
+      resultArray_b.push(collection[i]);
+    }
+  }
+  
+  return resultArray_a.sort(com_a).concat(resultArray_b.sort(com_b));
 };
+function com_a(a, b) {
+  return a - b;
+
+}
+function com_b(a, b) {
+  return b - a;
+}
 module.exports = even_asc_odd_desc;
