875068c1a3c155dddafaf233bde4e316548177cb
821453366 <821453366@qq.com>
2017-05-09 15:14:33 +0800

    提交第52题

--- practices/superposition_operation/interval_operation/amount_even.js
+++ practices/superposition_operation/interval_operation/amount_even.js
@@ -1,8 +1,13 @@
 'use strict';
 
 function amount_even(collection) {
-
-  //在这里写入代码
+  var sum = 0;
+  for (var index in collection) {
+    if (collection[index] % 2 === 0) {
+      sum += collection[index];
+    }
+  }
+  return sum;
 }
 
 module.exports = amount_even;
