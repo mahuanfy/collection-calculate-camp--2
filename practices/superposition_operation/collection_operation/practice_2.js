24bf649d92a80224d8fdf6cf9987d0e4c926f492
821453366 <821453366@qq.com>
2017-05-09 15:08:46 +0800

    提交第50题

--- practices/superposition_operation/collection_operation/practice_2.js
+++ practices/superposition_operation/collection_operation/practice_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function hybrid_operation_to_uneven(collection) {
+  var resultArray = [];
 
-  //在这里写入代码
+  for (var index in collection) {
+    if (collection[index] % 2 !== 0) {
+      resultArray.push(collection[index]*3+2);
+    }
+  }
+  return resultArray;
 }
 
 module.exports = hybrid_operation_to_uneven;
