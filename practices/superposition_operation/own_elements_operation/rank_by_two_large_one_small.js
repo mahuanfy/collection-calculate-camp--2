1368458ea3deece056ed19bfd796cc7cbfa2421f
821453366 <821453366@qq.com>
2017-05-09 21:39:58 +0800

    第64题目

--- practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
+++ practices/superposition_operation/own_elements_operation/rank_by_two_large_one_small.js
@@ -1,5 +1,19 @@
 'use strict';
-function rank_by_two_large_one_small(collection){
-  //这里写代码。。。
+function rank_by_two_large_one_small(collection) {
+  collection.sort(com_a);
+
+  for (var index in collection) {
+    if ((index + 1) % 3 === 0) {
+      let a = collection[index - 2];
+      collection[index - 2] = collection[index - 1];
+      collection[index - 1] = collection[index];
+      collection[index] = a;
+    }
+  }
+  return collection;
+}
+function com_a(a, b) {
+  return a - b;
+
 }
 module.exports = rank_by_two_large_one_small;
