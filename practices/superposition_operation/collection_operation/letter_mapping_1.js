34a4c12eea1d197222e1bb595de5f02c9ddac87f
821453366 <821453366@qq.com>
2017-05-09 14:58:59 +0800

    提交第48题

--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -2,12 +2,14 @@
 
 function even_to_letter(collection) {
   var result = [];
+
   var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
   for (var index in collection){
     if(collection[index]%2===0){
       result.push(letter[collection[index]]);
     }
   }
+
   return result;
 }
 
--- practices/superposition_operation/collection_operation/letter_mapping_2.js
+++ practices/superposition_operation/collection_operation/letter_mapping_2.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function average_to_letter(collection) {
+  var sum =0;
+  var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-  //在这里写入代码
+  for (var index in collection){
+    sum+=collection[index];
+  }
+
+  return letter[Math.ceil(sum/collection.length)];
 }
 
 module.exports = average_to_letter;
