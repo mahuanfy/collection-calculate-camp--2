3a2a792435f69488c854fdd14c579b23a8f8a16f
821453366 <821453366@qq.com>
2017-05-09 14:37:36 +0800

    提交第47题

--- practices/collectionOperator/get_letter_interval_2.js
+++ practices/collectionOperator/get_letter_interval_2.js
@@ -4,7 +4,6 @@ function get_letter_interval_2(number_a, number_b) {
   //在这里写入代码
   var result = [];
   var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-
   if (number_a < number_b) {
     for (var i = number_a; i <= number_b; i++) {
       if (i % 26 === 0) {
--- practices/superposition_operation/collection_operation/letter_mapping_1.js
+++ practices/superposition_operation/collection_operation/letter_mapping_1.js
@@ -1,8 +1,14 @@
 'use strict';
 
 function even_to_letter(collection) {
-
-  //在这里写入代码
+  var result = [];
+  var letter = ["", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+  for (var index in collection){
+    if(collection[index]%2===0){
+      result.push(letter[collection[index]]);
+    }
+  }
+  return result;
 }
 
 module.exports = even_to_letter;
